15b3df02e41aa61cdcaaf625997dacc3205f3cf4
kinghost <kingraser@gmail.com>
2016-10-11 17:10:44 +0800

    业务线

---
+++ src/main/java/com/sogou/iplus/entity/BusinessUnit.java
@@ -0,0 +1,15 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2015 Sogou.com. All Rights Reserved.
+ */
+package com.sogou.iplus.entity;
+
+//--------------------- Change Logs----------------------
+//@author wangwenlong Initial Created at 2016年10月11日;
+//-------------------------------------------------------
+public enum BusinessUnit {
+
+    desktop, search, marketing, sugarcat;
+
+}
