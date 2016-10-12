6a5402d13df7b6f428e4c991a51888644af5e3e8
kinghost <kingraser@gmail.com>
2016-10-12 16:21:51 +0800

    kpi logic

--- src/main/java/com/sogou/iplus/entity/BusinessUnit.java
+++ src/main/java/com/sogou/iplus/entity/BusinessUnit.java
@@ -5,11 +5,14 @@
  */
 package com.sogou.iplus.entity;
 
+import org.jsondoc.core.annotation.ApiObject;
+
 //--------------------- Change Logs----------------------
 //@author wangwenlong Initial Created at 2016年10月11日;
 //-------------------------------------------------------
+@ApiObject(name = "BusinessUnit", description = "业务线")
 public enum BusinessUnit {
 
-    desktop, search, marketing, sugarcat;
+  desktop, search, marketing, sugarcat;
 
 }
