691450524c77d72cfedafbb73111b16f55b473c3
kinghost <kingraser@gmail.com>
2016-10-13 14:02:02 +0800

    time -> date

--- model.sql
+++ model.sql
@@ -6,6 +6,6 @@ CREATE TABLE `kpi` (
   `xmId` INT(10) NOT NULL COMMENT '项目Id',  
   `kpiId` INT(10) NOT NULL COMMENT 'kpiId',
   `kpi` DECIMAL(12,2) NOT NULL COMMENT 'kpi',
-  `createDate` TIMESTAMP NOT NULL DEFAULT 0 COMMENT 'kpi时间',
+  `createDate` DATE NOT NULL DEFAULT 0 COMMENT 'kpi时间',
   UNIQUE KEY `xmid_kpiid_idx` (`xmId`, `kpiId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
