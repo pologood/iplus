857aae7810e08435c74a938c5e55476f7623db4d
kinghost <kingraser@gmail.com>
2016-12-29 16:32:55 +0800

    update

--- src/main/java/com/sogou/iplus/entity/Permission.java
+++ src/main/java/com/sogou/iplus/entity/Permission.java
@@ -12,12 +12,12 @@ public class Permission {
   public static final Map<String, Set<Integer>> MAP = new HashMap<>();
 
   static {
-    MAP.put("wangwenlong", BusinessUnit.SUGARCAT.getProjects().stream().flatMap(project -> project.getKpis().stream())
-        .map(kpi -> kpi.getKpiId()).collect(Collectors.toSet()));
+    MAP.put("xiaop_wangwenlong", BusinessUnit.SUGARCAT.getProjects().stream()
+        .flatMap(project -> project.getKpis().stream()).map(kpi -> kpi.getKpiId()).collect(Collectors.toSet()));
   }
 
   public static boolean isAuthorized(String userId, List<Integer> kpiIds) {
-    return MAP.getOrDefault("xiaop_" + userId, new HashSet<>(kpiIds)).containsAll(kpiIds);
+    return MAP.getOrDefault(userId, new HashSet<>(kpiIds)).containsAll(kpiIds);
   }
 
 }
