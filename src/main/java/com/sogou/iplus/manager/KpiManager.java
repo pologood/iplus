cbee8143cd78b9ba63f220e1295927a29bbdca1d
kinghost <kingraser@gmail.com>
2017-01-19 13:34:51 +0800

    update

--- src/main/java/com/sogou/iplus/manager/KpiManager.java
+++ src/main/java/com/sogou/iplus/manager/KpiManager.java
@@ -7,10 +7,8 @@ package com.sogou.iplus.manager;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.AbstractMap;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -61,21 +59,16 @@ public class KpiManager {
 
   public ApiResult<?> selectWithDateAndXmId(Integer xmId, LocalDate date) {
     List<Kpi> kpis = select(xmId, null, date);
-    return new ApiResult<>(kpis.stream()
-        .sorted((k1, k2) -> Double.compare(Project.getSortKpiId(k1.getKpiId()), Project.getSortKpiId(k2.getKpiId())))
-        .collect(Collectors.toMap(k -> k.getKpiId(), k -> k.getKpi(), (u, v) -> u, LinkedHashMap::new)));
-
+    return new ApiResult<>(kpis.stream().collect(Collectors.toMap(k -> k.getKpiId(), k -> k.getKpi())));
   }
 
   public ApiResult<?> selectWithDateRangeAndKpiId(Integer xmId, List<Integer> kpiId, LocalDate beginDate,
       LocalDate endDate) {
     List<Kpi> kpis = select(xmId, kpiId, beginDate, endDate);
-    Map<Integer, Map<LocalDate, Kpi>> result = new HashMap<>();
+    Map<Integer, Map<LocalDate, Kpi>> result = new TreeMap<>();
     kpis.forEach(kpi -> result.computeIfAbsent(kpi.getKpiId(), k -> new TreeMap<>(Comparator.reverseOrder()))
         .put(kpi.getCreateDate(), kpi));
-    return new ApiResult<>(result.entrySet().stream()
-        .sorted((e1, e2) -> Double.compare(Project.getSortKpiId(e1.getKey()), Project.getSortKpiId(e2.getKey())))
-        .collect(Collectors.toMap(e -> e.getKey(), e -> e.getValue(), (u, v) -> u, LinkedHashMap::new)));
+    return new ApiResult<>(result);
   }
 
   public ApiResult<?> getAverage(Integer xmId, List<Integer> kpiId, LocalDate beginDate, LocalDate endDate) {
